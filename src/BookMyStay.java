2efb81f5e5571d0ef511e4506d7a5569c1691743
sheetam522 <idhazhiya@gmail.com>
2026-03-13 08:23:35 +0530

    UC1:Application Entry & Welcome Message

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,13 @@
 public class BookMyStay {
+
+    /**
+     * Application entry point.
+     * This method is the first method executed
+     * when the program is launched by the JVM.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
 }
